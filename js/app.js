2daabbf75085e56bb5af4a7fe60a6475ae811e72
dogus <karssitetelekom@gmail.com>
2025-12-17 01:31:49 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -42,7 +42,7 @@ function showGlobalError(msg){
 }
 
 // Apps Script URL'si
-let SCRIPT_URL = localStorage.getItem("PUSULA_SCRIPT_URL") || ""; // Apps Script Web App URL
+let SCRIPT_URL = localStorage.getItem("https://script.google.com/macros/s/AKfycbywdciHyiPCEWGu9hIyN05HkeBgwPlFgzrDZY16K08svQhTcvXhN8A_DyBrzO8SalDu/exec") || ""; // Apps Script Web App URL
 
 // Oyun Değişkenleri
 let jokers = { call: 1, half: 1, double: 1 };
